651be817958255e2008781bd0cfb78327b0f955d
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-09-13 00:08:40 +0530

    Time: 2 ms (54.66%), Space: 59.9 MB (72.49%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let i=0,j=0;
+    while(i<nums.length){
+        if(nums[i]!=0){
+            let temp;
+            temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+        }
+        i++;
+    }
+};
