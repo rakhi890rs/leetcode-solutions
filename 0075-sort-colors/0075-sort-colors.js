87b6a91815f6843b02f2e2340c55904763589997
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-09-16 11:53:30 +0530

    Time: 0 ms (100%), Space: 55.1 MB (17.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,5 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    nums.sort();
+    let i=0,j=0,k=nums.length-1;
+    while(i<=k){
+        if(nums[i]===0){
+            [nums[i],nums[j]]=[nums[j],nums[i]];
+            i++;
+            j++;
+        }else if(nums[i]===2){
+            [nums[i],nums[k]]=[nums[k],nums[i]];
+            k--;
+        }else{
+            i++;
+        }
+    }
 };
