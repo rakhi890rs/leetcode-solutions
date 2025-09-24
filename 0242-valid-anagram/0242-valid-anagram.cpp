77ae23d81c47b6031aa41e4ce7e997d49e226537
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-09-24 13:13:23 +0530

    Time: 7 ms (20.3%), Space: 9.7 MB (74.3%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s.length()!=t.length()){
+            return false;
+        }
+        if(s==t){
+            return true;
+        }
+        return false;
+    }
+};
