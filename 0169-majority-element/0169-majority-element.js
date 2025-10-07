c2d4b0ea4e89be0bcd08dbb5a2c24819c8d22a62
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-10-07 13:23:35 +0530

    Time: 1624 ms (5%), Space: 57.2 MB (59.7%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let n=nums.length;
+    for(let i=0;i<n;i++){
+        let count=0;
+        for(let j=0;j<n;j++){
+            if(nums[i]==nums[j]){
+                count++;
+            }
+        }
+        if(count>Math.floor(n/2)){
+            return nums[i];
+        }
+    }
+};
