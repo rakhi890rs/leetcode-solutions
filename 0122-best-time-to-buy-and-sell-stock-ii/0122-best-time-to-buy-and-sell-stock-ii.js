4f31d6e517200a6f8aa6481ac1e830c5ef61014d
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-10-07 11:59:14 +0530

    Time: 2 ms (32.23%), Space: 55.9 MB (17.87%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let total=0;
+    for(let i=0;i<prices.length;i++){
+    if(prices[i]>prices[i-1])
+    total+=prices[i]-prices[i-1];
+    }
+    return total;
+
+};
