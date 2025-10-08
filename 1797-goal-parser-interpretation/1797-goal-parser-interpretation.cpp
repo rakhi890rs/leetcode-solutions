cf25ae653e3f4bf6df0bdfa703639c9071b3ba23
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-10-08 19:04:53 +0530

    Time: 4 ms (7.81%), Space: 8.3 MB (48.64%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.cpp
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    string interpret(string command) {     
-        string ans = "";
-        for (int i = 0; i < command.length(); i++) {
-            if (command[i] == 'G') {
-                ans += 'G';
-            } 
-            else if (command[i] == '(' && command[i + 1] == ')') {
-                ans += 'o';
-                i++;
-            } 
-            else if (command[i] == '(' && command[i + 1] == 'a') {
-                ans += "al";
-                i += 3; 
+    string interpret(string command) {
+        
+            while(command.find("()")!=string::npos){
+                command.replace(command.find("()"),2,"o");
             }
-        }
-        return ans;
+            while(command.find("(al)")!=string::npos){
+                command.replace(command.find("(al)"),4,"al");
+            }
+        return command;
     }
 };
