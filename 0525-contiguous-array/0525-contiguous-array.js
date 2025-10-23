7309f601c866e6e5c3fd62c39179f6dc08cac548
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-10-23 13:31:47 +0530

    Time: 25 ms (25.37%), Space: 64.5 MB (25.37%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function(nums) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==0) nums[i]=-1;
+    }
+    let k=0,sum=0,longest=0;
+    let map =new Map();
+    map.set(sum,-1);
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+        if(map.has(sum-k)){
+            longest=Math.max(longest,i-map.get(sum-k))
+        }
+        if(!map.has(sum))map.set(sum,i);
+    }
+    return longest;
+};
