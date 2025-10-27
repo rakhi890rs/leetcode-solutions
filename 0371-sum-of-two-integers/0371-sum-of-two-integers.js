d108e7130fbdf14a949e224be66e13ad5e161adb
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-10-27 22:58:05 +0530

    Time: 0 ms (100%), Space: 54.5 MB (14.39%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    let sum=0;
+    let carry=0;
+    let result = a+b;
+    return result;
+};
