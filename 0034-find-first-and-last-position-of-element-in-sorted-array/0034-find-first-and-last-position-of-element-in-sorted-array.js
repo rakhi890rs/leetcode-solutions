be92f30ed927a898e8d8d2557cd24be66efb4766
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-11-11 20:23:27 +0530

    Time: 0 ms (100%), Space: 55.2 MB (63.17%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,13 +3,24 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
-    let f=-1,l=-1;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===target){
-            if(f==-1)f=i;
-            l=i;
-        }
+
+var binary=function(nums,target,isStart){
+    let first =0,last=nums.length-1,ans=-1;
+    while(first<=last){
+        let mid=Math.floor(first+(last-first)/2);
+        if(nums[mid]==target){
+            ans=mid;
+            if(isStart) last=mid-1;
+            else first =mid+1;
     }
-    return [f,l];
+        else if(nums[mid]<target) first=mid+1;
+        else last=mid-1;
+    }
+      return ans;
+} 
+var searchRange = function(nums, target) {
+    let start = binary(nums,target,true)
+    let end=binary(nums,target,false)
+    return [start,end];
+   
 };
