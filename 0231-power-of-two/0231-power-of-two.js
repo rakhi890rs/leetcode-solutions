69b2468b4eae6c787b9df1371c61f4763ddb3be3
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-12-02 17:01:34 +0530

    Time: 1 ms (29.04%), Space: 55.1 MB (94.61%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n<1){
+        return 0;
+    }
+    while(n!=1){
+        if(n%2==1){
+            return 0;
+        }
+        n/=2;
+    }
+    return 1;
+};
