8d3a575dcd8eef2d08be2171246bf3bc8c932dff
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2025-12-11 20:50:15 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.35%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-     long long i = 0;
-        while(i*i<=x){
-         i++;   
+    if(x<2){
+        return x;
+    }
+     int start=0,end=x,mid,ans;
+     while(start<=end){
+        mid=start+(end-start)/2;
+        if(mid==x/mid){
+            ans=mid;
+            break;
+        }else if(mid<x/mid){
+            ans=mid;
+            start=mid+1;
+        }else{
+            end=mid-1;
         }
-        return i-1;
+     }return ans;
     }
 };
