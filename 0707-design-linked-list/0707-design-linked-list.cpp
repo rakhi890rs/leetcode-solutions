586f2f7d657c32ee84fec1ce9f91c28f4bd85618
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2026-01-23 21:54:17 +0530

    Time: 8 ms (54.3%), Space: 25.9 MB (93.89%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.cpp
@@ -0,0 +1,89 @@
+class MyLinkedList {
+public:
+    class Node {
+    public:
+        int data;
+        Node* next;
+        Node(int val) {
+            data = val;
+            next = NULL;
+        }
+    };
+
+    Node* head;
+    int size;
+
+    MyLinkedList() {
+        head = NULL;
+        size = 0;
+    }
+
+    int get(int index) {
+        if (index < 0 || index >= size) return -1;
+
+        Node* temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp->next;
+        }
+        return temp->data;
+    }
+
+    void addAtHead(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+        size++;
+    }
+
+    void addAtTail(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+        size++;
+    }
+
+    void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+
+        Node* temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp->next;
+        }
+
+        Node* newNode = new Node(val);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        size++;
+    }
+
+    void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+
+        if (index == 0) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        } else {
+            Node* temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp->next;
+            }
+            Node* del = temp->next;
+            temp->next = del->next;  
+            delete del;
+        }
+        size--;
+    }
+};
