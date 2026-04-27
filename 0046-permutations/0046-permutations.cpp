4c2331c8603e92ccd86292e30f8550367af3944b
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2026-04-27 13:04:38 +0530

    Time: 0 ms (100%), Space: 12.8 MB (7.31%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,24 @@
 class Solution {
-public:   
-    void solve(vector<int>& nums, int index, vector<vector<int>>& ans) {
-        if (index == nums.size()) {
-            ans.push_back(nums);
-            return;
-        }      
-        for (int i = index; i < nums.size(); i++) {      
-            swap(nums[index], nums[i]);      
-            solve(nums, index + 1, ans);     
-            swap(nums[index], nums[i]);     
-        }
-    } 
-    vector<vector<int>> permute(vector<int>& nums) {    
-        vector<vector<int>> ans;
-        solve(nums, 0, ans);
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        vector<bool>visited(nums.size(),0);
+        solve(nums,ans,temp,visited);
         return ans;
+    }
+    void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int>temp,vector<bool>visited){
+        if(temp.size()==nums.size()){
+            ans.push_back(temp);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(visited[i]==0){
+                temp.push_back(nums[i]);
+                visited[i]=1;
+                solve(nums,ans,temp,visited);
+                visited[i]=0;
+                temp.pop_back();
+            }
+        }
     }
 };
