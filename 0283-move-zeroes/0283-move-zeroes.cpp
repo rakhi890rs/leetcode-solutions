97410a3a70313b2b7b6d970d29908aa5ce88885e
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2026-08-15 16:19:13 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       fill(remove(nums.begin(),nums.end(),0),nums.end(),0);
+        int i=0,j=0;
+        while(i<nums.size()){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+            i++;
+        }
     }
 };
