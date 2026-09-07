fa9481fe68257038be322772ee6ebf50692e2573
Rakhi singh <149752236+rakhi890rs@users.noreply.github.com>
2026-09-07 14:35:55 +0530

    Time: 3 ms (95.87%), Space: 19.4 MB (82.15%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+         if (nums.size()<= 2) return nums.size();
         int j=2;
-        if(nums.size()<=2) return nums.size();
         for(int i=2;i<nums.size();i++){
             if(nums[i]!=nums[j-2]){
                 nums[j]=nums[i];
@@ -10,5 +10,6 @@ public:
             }
         }
         return j;
+
     }
 };
